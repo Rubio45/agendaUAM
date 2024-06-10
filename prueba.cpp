d2987227f2e56fbcfe003ce7e7dd18ec5c3e4c33
Rubio45 <aleshdiaz45@gmail.com>
2024-06-10 07:28:59 -0600

    "Todo esta casi listo"

--- prueba.cpp
+++ prueba.cpp
@@ -47,60 +47,6 @@ void guardarDatosActividad(ActividadDatos& actividad) {
     }
 }
 
-void eliminarActividad(const string nombreUsuario){
-    string nombreArchivo = "actividades" + nombreUsuario + ".txt";
-    int numero;
-    bool existeNumero;
-    ActividadDatos registro_actividades;
-    ifstream archivo(nombreArchivo);
-    cout << "\n\n--------------------------------------------------------\nIntroduzca el numero de actividad que desea eliminar\n--------------------------------------------------------\nNumero: ";
-    cin >> numero;
-    if (archivo.is_open()) {
-        string linea;
-        while (getline(archivo, linea)) {
-            stringstream ss(linea);
-            string numero_actividad, nombre_actividad, descripcion_actividad, lugar_actividad, fecha_actividad, hora_inicio, hora_final;
-            getline(ss, numero_actividad, ',');
-            registro_actividades.numero_actividad = stringToInt(numero_actividad);
-            if (registro_actividades.numero_actividad != numero){
-            existeNumero = false;
-            getline(ss, nombre_actividad, ',');
-            getline(ss, descripcion_actividad, ',');
-            getline(ss, lugar_actividad, ',');
-            getline(ss, fecha_actividad, ',');
-            getline(ss, hora_inicio, ',');
-            getline(ss, hora_final, ',');
-
-            size_t primer_slash = fecha_actividad.find('/');
-            size_t segundo_slash = fecha_actividad.find('/', primer_slash + 1);
-
-            registro_actividades.nombre_actividad = nombre_actividad;
-            registro_actividades.descripcion_actividad = descripcion_actividad;
-            registro_actividades.lugar_actividad = lugar_actividad;
-            registro_actividades.fecha_actividad.dia = stringToInt(fecha_actividad.substr(0, primer_slash));
-            registro_actividades.fecha_actividad.mes = stringToInt(fecha_actividad.substr(primer_slash + 1, segundo_slash-primer_slash-1));
-            registro_actividades.fecha_actividad.annio = stringToInt(fecha_actividad.substr(segundo_slash+1));
-            registro_actividades.hora_inicio = stringToInt(hora_inicio);
-            registro_actividades.hora_final = stringToInt(hora_final);
-            guardarDatosActividad(registro_actividades);
-            //limpiar pantalla
-            }
-            if (existeNumero)
-            {
-                // limpiar pantalla
-                cout<<"El numero de la actividad que proporcionaste no existe";
-                // esperar
-            }
-        }
-        cout << "Tarea eliminada";
-            // esperar
-        archivo.close();
-        remove(nombreArchivo.c_str());
-        rename("actividades.txt", nombreArchivo.c_str());
-
-    }
-}
 int main (){    
-    eliminarActividad("Alex");
     return 0; 
 }
